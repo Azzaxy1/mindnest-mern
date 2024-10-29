641dd23b88f5afd428e062e0771f0083b9578ad6
Azzaxy <abdurrohmanazis@gmail.com>
2024-10-29 13:54:46 +0700

    feat: add detailBlog page

--- fe-mern-nexablog/src/pages/DetailBlog/index.tsx
+++ fe-mern-nexablog/src/pages/DetailBlog/index.tsx
@@ -1,7 +1,18 @@
+import { RegisterBg } from "../../assets";
+import "./detailBlog.scss";
+
 const DetailBlog = () => {
   return (
-    <div>
-      <p>DetailBlog Page</p>
+    <div className="detail-blog-wrapper">
+      <img className="img-cover" src={RegisterBg} alt="thumb" />
+      <p className="blog-title">Title</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        possimus error id magni odio provident, debitis repellat? Sint, quidem
+        amet Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+        modi similique dolorem ducimus repudiandae perferendis!.
+      </p>
     </div>
   );
 };
