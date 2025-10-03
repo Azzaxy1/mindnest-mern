ed657fa541073313eaa42acaaaeec575491c350d
Azzaxy <abdurrohmanazis@gmail.com>
2025-10-04 01:28:45 +0700

    fix: update MongoDB connection handling and improve error logging

--- be-mern-nexablog/src/config/db.config.ts
+++ be-mern-nexablog/src/config/db.config.ts
@@ -2,27 +2,20 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const uri: string =
-      process.env.MONGO_URI || "mongodb://localhost:27017/mindnest";
+    const uri: string = process.env.MONGO_URI || "";
 
-    // Opsi koneksi untuk menangani masalah koneksi
     const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Timeout setelah 5 detik
-      socketTimeoutMS: 45000, // Timeout socket setelah 45 detik
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
     };
 
     await mongoose.connect(uri, options);
-    console.log("MongoDB connected Successfully");
+    console.log("✅ MongoDB connected Successfully");
   } catch (error) {
-    console.log("MongoDB connection failed", error);
+    console.error("❌ MongoDB connection failed:", error);
     console.log("Retrying connection in 5 seconds...");
 
-    // Retry koneksi setelah 5 detik
-    setTimeout(() => {
-      connectDB();
-    }, 5000);
+    setTimeout(connectDB, 5000);
   }
 };
 
